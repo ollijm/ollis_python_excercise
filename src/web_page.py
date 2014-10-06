db7a6366d7b65d709be5ad2a2ddadddec1375e8d
Olli Miettinen <ollijm@gmail.com>
2014-10-06 17:56:51 +0300

    Add WebPage class to hold the state of the monitored web page

---
+++ src/web_page.py
@@ -0,0 +1,58 @@
+__author__ = 'olli'
+
+
+class WebPage:
+    """
+    WebPage represents the state of a web resource that we monitor
+    """
+    def __init__(self, url, expected_status_code, expected_content_type):
+        self._url = url
+        self._expected_status_code = expected_status_code
+        self._actual_status_code = None
+        self._expected_content_type = expected_content_type
+        self._actual_content_type = None
+        self._response_time = None
+        self._last_checked = None
+
+
+    def __str__(self):
+        s = "url='{0}' " \
+            "status_code='{1}' " \
+            "expected_status_code='{2}' " \
+            "content_type='{3}' " \
+            "expected_content_type='{4}' " \
+            "response_time='{5}' " \
+            "last_checked='{6}'".format(self.url, self.actual_status_code, self.expected_status_code,
+                                        self.actual_content_type, self.expected_content_type, self.response_time,
+                                        self.last_checked)
+        return s
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def expected_status_code(self):
+        return self._expected_status_code
+
+    @property
+    def actual_status_code(self):
+        return self._actual_status_code
+
+    @property
+    def expected_content_type(self):
+        return self._expected_content_type
+
+    @property
+    def actual_content_type(self):
+        return self._actual_content_type
+
+    @property
+    def response_time(self):
+        """In milliseconds"""
+        return self._response_time
+
+    @property
+    def last_checked(self):
+        return self._last_checked
+
