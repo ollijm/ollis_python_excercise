4b124e8c2ebdc57fac440d91b7552e330911879a
Olli Miettinen <ollijm@gmail.com>
2014-10-06 21:04:34 +0300

    Move feed validation into validator.py and refactor variable names

--- src/poller.py
+++ src/poller.py
@@ -7,58 +7,58 @@ import time
 import requests
 import requests.exceptions
 import web_page
+import validator
 
 _logger = logging.getLogger('poller')
 _logger.setLevel(logging.INFO)
 
 
-def update(webpage):
+def update(page):
     """
     Update the status of given WebPage
-    :type webpage: web_page.WebPage
-    :param webpage: WebPage object
+    :type page: web_page.WebPage
+    :param page: WebPage object
     :return:
     """
-    webpage.last_checked = datetime.datetime.now()
+    page.last_checked = datetime.datetime.now()
     try:
         start_at = time.time()
-        request = _read(webpage.url)
+        request = _read(page.url)
         end_at = time.time()
-        print "HERE"
-        _set_stats(webpage, request, int(round((end_at - start_at) * 1000)))
+        _set_stats(page, request, int(round((end_at - start_at) * 1000)))
     except requests.exceptions.RequestException, e:
         # Catch any exception from 'requests' library
         # TODO: separate handling of different connection or HTTP parsing errors
-        _set_fail_stats(webpage, e)
-        print "Exception on  " + webpage.url + " EXCEPTION: " + str(e) + " TYPE: " + str(type(e))
+        _set_fail_stats(page, e)
 
-    _validate_results(webpage)
+    _validate_results(page)
 
 
 def _read(url):
     return requests.get(url, )
 
 
-def _set_stats(webpage, request, response_time):
-    webpage.connection_state = "OK"
-    webpage.response_time = response_time
-    webpage.actual_status_code = request.status_code
-    webpage.actual_content_type = request.headers['Content-Type']
+def _set_stats(page, request, response_time):
+    page.connection_state = "OK"
+    page.response_time = response_time
+    page.actual_status_code = request.status_code
+    page.actual_content_type = request.headers['Content-Type']
 
 
-def _set_fail_stats(webpage, error):
-    webpage.connection_state = str(type(error))
-    webpage.response_time = None
-    webpage.actual_status_code = None
-    webpage.actual_content_type = None
+def _set_fail_stats(page, error):
+    print "Setting error on page " + page.url
+    page.connection_state = error.__class__.__name__
+    page.response_time = None
+    page.actual_status_code = None
+    page.actual_content_type = None
+    print "FINALLY: " + str(page)
 
 
-def _validate_results(webpage):
-    if webpage.connection_state is not "OK" or webpage.actual_status_code is not webpage.expected_status_code \
-            or not webpage.actual_content_type.startswith(webpage.expected_content_type):
-        test_status = "FAIL"
-    else:
+def _validate_results(page):
+    if validator.validate_web_page(page):
         test_status = "OK"
+    else:
+        test_status = "FAIL"
 
     # Finally log the poll event results to file
-    _logger.info("{0} {1}".format(test_status, str(webpage)))
+    _logger.info("{0} {1}".format(test_status, str(page)))
--- src/validator.py
+++ src/validator.py
@@ -25,3 +25,11 @@ def validate_result(s):
         return True
     except ValueError:
         return False
+
+
+def validate_web_page(webpage):
+    if webpage.connection_state is not "OK" or webpage.actual_status_code is not webpage.expected_status_code \
+            or not webpage.actual_content_type.startswith(webpage.expected_content_type):
+        return False
+    else:
+        return True
