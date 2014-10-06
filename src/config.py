7c6521b9cbefc395cf828a57859074d3872697d9
Olli Miettinen <ollijm@gmail.com>
2014-10-06 18:02:56 +0300

    Add dummy Config class (which will later on read config from file)

--- src/config.py
+++ src/config.py
@@ -1,1 +1,45 @@
 __author__ = 'olli'
+
+
+import unittest
+import web_page
+
+
+class Config:
+
+    def __init__(self, config_file):
+        """
+        :param config_file:
+        :return:
+        """
+        self._config_file = config_file
+        self.url_targets = []
+
+    @property
+    def config_file(self):
+        return self._config_file
+
+    @property
+    def url_targets(self):
+        return self._url_targets
+
+    def initialize(self):
+        # TODO: Read from file
+        u1 = web_page.WebPage("http://www.kaleva.fi/", 200, "text/html")
+        u2 = web_page.WebPage("http://www.iltasanomat.fi/rss/uutiset.xml", 200, "text/xml")
+        u3 = web_page.WebPage("http://www.google.fi/", 200, "text/html")
+
+        self.url_targets.append(u1)
+        self.url_targets.append(u2)
+        self.url_targets.append(u3)
+
+
+class ConfigTest(unittest.TestCase):
+    def test_instantiate(self):
+        config = Config("/path/to/file")
+        config.initialize()
+        for url_target in config.url_targets:
+            print str(url_target)
+
+if __name__ == '__main__':
+    unittest.main()
